da831a5ad7fa55b1588bd9db4b7ac44ce9888c53
Saul Vieira <vieira_saul@yahoo.com.br>
2020-01-28 20:28:43 -0300

    configuring react router

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './app.js';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'; 
 // import './index.css';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<BrowserRouter>
+		<App />
+	</ BrowserRouter>
+	,document.getElementById('root'));
