88798b554df437d8c66c0df2793eb56fe77a5ced
Saul Vieira <vieira_saul@yahoo.com.br>
2020-01-30 17:52:18 -0300

    some layout alterations

--- src/Components/ExperienceBlock/Experience-block.js
+++ src/Components/ExperienceBlock/Experience-block.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import styles from './Experience-block.module.scss';
 
-const ExperienceBlock = ({children, children_2}) => {
+const ExperienceBlock = ({children}) => {
 	return (
 		<div>
-			<h2 className={styles.estilo_aqui}>
-				{children}
-			</h2>
-            <h3 className={styles.estilo_aqui}>
-				{children_2}
-			</h3>
+            {children}
 		</div>
 	);
 }
