87997b7dd3a76264bd805e8e7ef7ffa58b896554
Saul Vieira <vieira_saul@yahoo.com.br>
2020-02-03 02:53:24 -0300

    Updates

---
+++ precache-manifest.0bb223c54a69da3fdcdecbe688e85ab3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b55a79d601871950893834792b24a32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8cee26726e8a4a56b9f",
+    "url": "/static/css/main.20ea3887.chunk.css"
+  },
+  {
+    "revision": "d662ed844ba8411006bd",
+    "url": "/static/js/2.19efdfe3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.19efdfe3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8cee26726e8a4a56b9f",
+    "url": "/static/js/main.aa1ee51e.chunk.js"
+  },
+  {
+    "revision": "9e27ea1699393084ed9c",
+    "url": "/static/js/runtime-main.ab5fa6df.js"
+  }
+]);
