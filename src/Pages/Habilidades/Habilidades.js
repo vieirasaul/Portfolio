b91f7bdf725149483716e4476ac1dcb8740e21dd
Saul Vieira <vieira_saul@yahoo.com.br>
2020-02-03 15:28:03 -0300

    skill chart component working with inline widths

--- src/Pages/Habilidades/Habilidades.js
+++ src/Pages/Habilidades/Habilidades.js
@@ -10,15 +10,15 @@ class Habilidades extends Component {
 					<h1>Habilidades</h1>
                     <SkillChart ><h2 style={{ width: "80%" }}>Html</h2></SkillChart>
                     <SkillChart ><h2 style={{ width: "70%" }}>Css</h2></SkillChart>
-                    <SkillChart ><h2 style={{ width: "50%" }}>JavaScript</h2></SkillChart>
-                    <SkillChart ><h2 style={{ width: "55%" }}>jQuery</h2></SkillChart>
-                    <SkillChart ><h2 style={{ width: "40%" }}>React</h2></SkillChart>
-                    <SkillChart ><h2 style={{ width: "50%" }}>Sass</h2></SkillChart>
-                    <SkillChart ><h2 style={{ width: "50%" }}>Git</h2></SkillChart>
+                    <SkillChart ><h2 style={{ width: "40%" }}>JavaScript</h2></SkillChart>
+                    <SkillChart ><h2 style={{ width: "50%" }}>jQuery</h2></SkillChart>
+                    <SkillChart ><h2 style={{ width: "20%" }}>React</h2></SkillChart>
+                    <SkillChart ><h2 style={{ width: "40%" }}>Sass</h2></SkillChart>
+                    <SkillChart ><h2 style={{ width: "40%" }}>Git</h2></SkillChart>
                     <SkillChart ><h2 style={{ width: "45%" }}>WordPress</h2></SkillChart>
 				</div>
 			</main>	
 		);
-	}
+    }
 }
 export default Habilidades;
