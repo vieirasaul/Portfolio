c0863d870ea91d0ea6430759056da91a2279f691
Saul Vieira <vieira_saul@yahoo.com.br>
2020-04-20 13:48:46 -0300

    portfolio grid consuming json with fetch api

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,39 +1,36 @@
 import React, {Component} from 'react';
 import styles from './Portfolio.module.scss';
 import cx from 'classnames';
-import PortfolioItem from '../../Components/PortfolioItem/PortfolioItem';
+import { Link } from 'react-router-dom';
 
 class Portfolio extends Component {
+    state = {
+        projects: []
+    };
+
+    componentDidMount() {
+        fetch('data/portfolio.json')
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    projects: res
+                });
+            });
+    }
+
 	render() {
 		return (
 			<main className={cx(styles.center_main, styles.portfolio)} >
 				<div className="container">
                     <h1>Portfolio</h1>
                     <div className={styles.grid_portfolio}>
-                        <PortfolioItem>
-                            <p>blublu</p>
-                        </PortfolioItem>  
-                        <PortfolioItem>
-                            <p>blublu</p>
-                        </PortfolioItem>  
-                        <PortfolioItem>
-                            <p>blublu</p>
-                        </PortfolioItem>  
-                        <PortfolioItem>
-                            <p>blublu</p>
-                        </PortfolioItem>  
-                        <PortfolioItem>
-                            <p>blublu</p>
-                        </PortfolioItem>  
-                        <PortfolioItem>
-                            <p>blublu</p>
-                        </PortfolioItem>  
-                        <PortfolioItem>
-                            <p>blublu</p>
-                        </PortfolioItem>  
-                        <PortfolioItem>
-                            <p>blublu</p>
-                        </PortfolioItem>   
+                        {this.state.projects.map(item => (
+                            <Link className={styles.grid_item} key={item.id} style={{ backgroundImage: `url(${item.image})` }}>
+                                <div className={styles.mask}>
+                                    <h2>{item.name}</h2>
+                                </div>
+                            </Link> 
+                        ))}
                     </div>
 				</div>
 			</main>	
